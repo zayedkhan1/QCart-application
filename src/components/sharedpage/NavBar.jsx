62072417242f549a77108b650115e8fdd84ba1c3
zayed khan <zayedk458@gmail.com>
2025-08-02 00:26:21 +0600

    done

--- src/components/sharedpage/NavBar.jsx
+++ src/components/sharedpage/NavBar.jsx
@@ -94,21 +94,34 @@ const NavBar = () => {
                             {
                                 open && (
                                     <div className='absolute top-full right-0 mt-2 flex flex-col bg-white shadow px-4 py-3 space-y-2 w-40 z-20'>
-                                        <a href="#">home</a>
-                                        <a href="#">menu</a>
-                                        <a href="#">mobileApp</a>
-                                        <a href="#">contact us</a>
-                                        <div className='space-y-2 '>
-                                            <div >
-                                                <input className='w-full p-1 bg-gray-300 rounded-sm' type="text" placeholder='serach' />
-                                            </div>
-                                            <div>
-                                                <p><FaShoppingCart /></p>
-                                            </div>
-                                            <div>
-                                                <button>login</button>
-                                            </div>
-                                        </div>
+                            <a href="/home">Home</a>
+                            <a href="/shop">Shop</a>
+                            <a href="#">About Us</a>
+                            <a href="#">Contact</a>
+                            <a href="/sideLayout"
+                            className='border-1 rounded-full p-1 border-gray-400 text-xs '>Seller Dashboard</a>
+                                          <div>
+                                <div className="flex items-center space-x-2">
+                                    <input
+                                        type="text"
+                                        value={query}
+                                        onChange={(e) => setQuery(e.target.value)}
+                                        placeholder="Search"
+                                        className={`transition-all w-[80px] duration-300 border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400
+                                          ${isOpen ? "w-64 opacity-100" : "w-0 opacity-0 px-0"} `}
+                                    />
+                                    <button
+                                        onClick={handleSearch}
+                                        className="p-2 text-gray-600 hover:text-orange-600 transition-colors cursor-pointer"
+                                        aria-label="Search"
+                                    >
+                                        <FiSearch size={20} />
+                                    </button>
+                                </div>
+                                <div>
+                                <button className="btn relative flex items-center gap- overflow-hidden px-3 py-1 border-2 rounded-full border-amber-600  cursor-pointer">Login</button>
+                            </div>
+                            </div>
                                     </div>
 
                                 )}
